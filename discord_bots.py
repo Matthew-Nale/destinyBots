fe383c4370ad6e499be747d15dc43f86562b6f37
Matthew-Nale <mnale@purdue.edu>
2023-10-01 01:04:55 -0400

    Changed to allow specific running of bots/tasks

--- discord_bots.py
+++ discord_bots.py
@@ -1,3 +1,4 @@
+import sys
 import asyncio
 from datetime import datetime
 from src.conversations import scheduledBotConversation
@@ -7,20 +8,36 @@ from bots.drifter import drifter
 
 #? Running bots
 
+async def get_tasks(bot_list:dict):
+    tasks = []
+    
+    for name in bot_list:
+        sys.stdout.write(f'Activate {name}? [Y/n] ')
+        if input().lower() == "y":
+            tasks.append(bot_list[name])
+            
+    sys.stdout.write(f'Enable daily bot conversations? [Y/n] ')
+    if input().lower() == "y":
+        tasks.append(scheduledBotConversation.start())
+
+    return tasks
+
 async def main():
     #* Create log.txt and provide date of creation
     log = open("log.txt", "w")
     log.write(f'Started bots at {datetime.now()}\n\n')
     log.close()
 
+    #* Make dict of bots and names
+    bot_list = {rhulk.name: rhulk.bot.start(rhulk.discord_token),
+                calus.name: calus.bot.start(calus.discord_token),
+                drifter.name: drifter.bot.start(drifter.discord_token)}
+    
+    #* Ask user for which bots/events to run
+    tasks = await get_tasks(bot_list)
 
-    #* Run bots until manually quit
-    await asyncio.gather(
-        calus.bot.start(calus.discord_token),
-        rhulk.bot.start(rhulk.discord_token),
-        drifter.bot.start(drifter.discord_token),
-        scheduledBotConversation.start(),
-    )
+    #* Run required tasks until complete
+    await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
     asyncio.run(main())
